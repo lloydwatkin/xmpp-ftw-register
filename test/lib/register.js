d9214d5f4dd1cf3416b5e2f1bb6ef533cb13a4a0
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-07-30 22:03:44 +0100

    Adding first unit tests

--- test/lib/register.js
+++ test/lib/register.js
@@ -1,5 +1,5 @@
 var should  = require('should')
-  , register = require('../../lib/register')
+  , Register = require('../../lib/register')
   , ltx     = require('ltx')
   , helper  = require('../helper')
 
@@ -20,8 +20,63 @@ describe('Register', function() {
                 this.callback(error, data)
             }
         }
-        register = new register()
+        register = new Register()
         register.init(manager)
+    })
+
+    describe('Get registration details', function() {
+
+        it('Errors when no callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.register.get', {})
+        })
+
+        it('Errors when non-function callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.register.get', {}, true)
+        })
+ 
+        it('Errors if missing \'to\' key', function(done) {
+            var request = {}
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'to' key")
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit(
+                'xmpp.register.get',
+                request,
+                callback
+            )
+        })
+
     })
 
 })
