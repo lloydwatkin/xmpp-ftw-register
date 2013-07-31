7c3f08e948e82adc0a4d8e66a96da3c799c04146
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-07-31 14:14:44 +0100

    Add tests and code for basic registration. Fixes #1.

--- test/lib/register.js
+++ test/lib/register.js
@@ -158,5 +158,141 @@ describe('Register', function() {
         })
 
     })
+    
+    describe('Can register', function() {
+
+        it('Errors when no callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.register.set', {})
+        })
+
+        it('Errors when non-function callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.register.set', {}, true)
+        })
+ 
+        it('Errors if missing \'to\' key', function(done) {
+            var request = {}
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'to' key")
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit(
+                'xmpp.register.set',
+                request,
+                callback
+            )
+        })
+
+        it('Sends expected stanza', function(done) {
+            var request = {
+                to: 'shakespeare.lit'
+            }
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.to.should.equal(request.to)
+                stanza.attrs.id.should.exist
+                stanza.attrs.type.should.equal('set')
+                stanza.getChild('query', register.NS)
+                    .should.exist
+                done()
+            })
+            socket.emit(
+                'xmpp.register.set',
+                request,
+                function() {}
+            )
+        })
+        
+        it('Sends expected stanza with fields', function(done) {
+            var request = {
+                to: 'shakespeare.lit',
+                username: 'romeo',
+                email: 'romeo@shakespeare.lit',
+                password: 'love-juliet'
+            }
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.to.should.equal(request.to)
+                stanza.attrs.id.should.exist
+                stanza.attrs.type.should.equal('set')
+                var query = stanza.getChild('query', register.NS)
+                query.should.exist
+                query.getChildText('username').should.equal(request.username)
+                query.getChildText('password').should.equal(request.password)
+                query.getChildText('email').should.equal(request.email)
+                done()
+            })
+            socket.emit(
+                'xmpp.register.set',
+                request,
+                function() {}
+            )
+        })
+
+        it('Handles error response stanza', function(done) {
+            xmpp.once('stanza', function(stanza) {
+                manager.makeCallback(helper.getStanza('iq-error'))
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.should.eql({
+                    type: 'cancel',
+                    condition: 'error-condition'
+                })
+                done()
+            }
+            var request = {
+                to: 'shakespeare.lit'
+            }
+            socket.emit('xmpp.register.set', request, callback)
+        })
+
+        it('Confirms expected registration', function(done) {
+            xmpp.once('stanza', function(stanza) {
+                manager.makeCallback(
+                    helper.getStanza('iq-result')
+                )
+            })
+            var callback = function(error, data) {
+                should.not.exist(error)
+                data.registered.should.be.true
+                done()
+            }
+            var request = {
+                to: 'shakespeare.lit'
+            }
+            socket.emit('xmpp.register.set', request, callback)
+        })
+        
+    })
 
 })
